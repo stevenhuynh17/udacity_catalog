a541f33a340b102d5d25710800de833e32aeddd5
Steven <stevenhuynh17@gmail.com>
2017-10-20 14:15:04 -0700

    Local server functioning

--- application.py
+++ application.py
@@ -2,4 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app.route('/')
+
+@app.route('/')
+def Hello():
+    return "IT WORKS"
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=8000)
